e2e578dacbc123ea3e77bf74509f951d675ee521
Thibaut Broggi <broggi_t@epitech.eu>
2015-10-06 01:17:42 +0200

    adding an API server

---
+++ server.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+var express = require('express');
+var Maria = require('mariasql');
+var config = require('./config.json');
+
+var db = new Maria(config.db);
+var app = express();
+
+function invalidRoute(req, res) {
+  res.statusCode = 400;
+  res.statusMessage = 'Invalid route',
+  res.send(res.statusMessage);
+}
+
+function serveApi(req, res) {
+  if (!req.params.promo.match(/^\d+$/))
+    invalidRoute(req, res);
+  console.log(req.params);
+  query = 'SELECT * FROM `Users` WHERE `promo` = :promo';
+  if (req.params.city) {
+    req.params.city = '%' + req.params.city;
+    query += ' AND `city` LIKE :city';
+  }
+  res.header('Content-Type', 'application/json')
+  db.query(query, req.params, function(e, r) {
+    res.send(r);
+  });
+}
+
+app.listen(config.port)
+app.get('/api/:promo', serveApi)
+  .get('/api/:promo/:city', serveApi)
+  .use(function(req, res) {
+    res.statusCode = 404;
+    res.statusMessage = 'Not found',
+    res.send(res.statusMessage);
+  })
